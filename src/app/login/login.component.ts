bb5a2d70dbfe76f1a32a495e632260e5b71dc6f9
akshitabansal24 <akshitabansal24@gmail.com>
2024-04-13 13:33:57 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Route } from '@angular/compiler/src/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationService } from '../registration.service';
+import { User } from '../user';
+import * as $ from 'jquery'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = new User();
+  msg = "";
+  adminEmail = "";
+  adminPassword = "";
+
+  constructor(private _service : RegistrationService, private _router : Router) { }
+
+  ngOnInit(): void 
+  { }
+
+  loginUser()
+  {
+      this._service.loginUserFromRemote(this.user.email,this.user.password).subscribe(
+        (data: any) => {
+          console.log(data);
+          console.log("Response Received");
+          sessionStorage.setItem('loggedUser', this.user.email);
+          sessionStorage.setItem('USER', "user");
+          sessionStorage.setItem('ROLE', "user");
+          // sessionStorage.setItem('USER', "admin");
+          // sessionStorage.setItem('ROLE', "admin");
+          this._router.navigate(['/userdashboard']);
+        },
+        (error: { error: any; }) => {
+          console.log(error.error);
+          this.msg="Bad credentials, please enter valid credentials !!!";
+        }
+      )
+  }
+
+}
